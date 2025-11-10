904e6f36792bf2ee83f15af66131a574c9580e0b
Velawn Ren <yaruren@VelawndeMacBook-Air.local>
2025-11-09 20:30:46 -0500

    Add say_goodbye() function

--- greetings.py
+++ greetings.py
@@ -1,5 +1,13 @@
 def say_hello():
     print("hello")
 
+def say_goodbye():
+    print("goodbye")
+
+if __name__ == "__main__":
+    say_hello()
+    say_goodbye()def say_hello():
+    print("hello")
+
 if __name__ == "__main__":
     say_hello() 
